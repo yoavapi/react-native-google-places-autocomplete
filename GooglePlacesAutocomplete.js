cd379f12edd70894d3384b2d4a461f09c4c2a91d
Yoav Prat <yoav.prat@gmail.com>
2018-08-03 10:30:47 +0300

    protect parse error: unterminated string literal, unexpected end of data

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -392,7 +392,15 @@ export default class GooglePlacesAutocomplete extends Component {
         }
 
         if (request.status === 200) {
-          const responseJSON = JSON.parse(request.responseText);
+        
+          let responseJSON;
+          try {
+            responseJSON = JSON.parse(request.responseText);
+          } catch (error) {
+            console.log("query:", encodeURIComponent(text) + '&' + Qs.stringify(this.props.query));
+            console.warn("unable to parse:", request.responseText);
+            return;
+          }
 
           this._disableRowLoaders();
 
@@ -461,7 +469,15 @@ export default class GooglePlacesAutocomplete extends Component {
         }
 
         if (request.status === 200) {
-          const responseJSON = JSON.parse(request.responseText);
+          let responseJSON;
+          try {
+            responseJSON = JSON.parse(request.responseText);
+          } catch (error) {
+            console.log("query:", encodeURIComponent(text) + '&' + Qs.stringify(this.props.query));
+            console.warn("unable to parse:", request.responseText);
+            return;
+          }
+          
           if (typeof responseJSON.predictions !== 'undefined') {
             if (this._isMounted === true) {
               const results = this.props.nearbyPlacesAPI === 'GoogleReverseGeocoding'
